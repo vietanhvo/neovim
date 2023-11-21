ade71fd959c3ca8ceb8eff6cb1e471d2e2507801
Viet Anh Vo <anhviet5121pc@gmail.com>
2023-11-21 11:20:44 +0700

    Update

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -11,7 +11,10 @@ return {
 
     vim.opt.termguicolors = true
 
-    vim.cmd([[ hi NvimTreeSpecialFile guifg=#ff7800 ]])
+    vim.cmd([[ 
+      hi NvimTreeSpecialFile guifg=#ff7800, 
+      hi NvimTreeWindowPicker gui=bold guibg=#83a598 guifg=#fbf1c7
+    ]])
 
     -- configure nvim-tree
     nvimtree.setup({
@@ -21,6 +24,9 @@ return {
       renderer = {
         highlight_git = true,
         group_empty = true,
+        indent_markers = {
+          enable = true,
+        },
       },
       update_focused_file = {
         enable = true,
@@ -33,7 +39,6 @@ return {
     local nt_api = require("nvim-tree.api")
 
     keymap.set('n', '<C-b>', nt_api.tree.toggle, { noremap = true, silent = true })
-    keymap.set('n', '<C-i>', nt_api.tree.change_root_to_node, { noremap = true, silent = true })
     keymap.set("n", "tf", nt_api.tree.focus)
   end,
 }
