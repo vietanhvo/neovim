144d16bb3ca947a06219d429e93de95b32016730
Viet Anh Vo <vietanhvo@pm.me>
2024-12-26 12:46:53 +0700

    Update

--- lua/config/plugins/mini-icons.lua
+++ lua/config/plugins/mini-icons.lua
@@ -1,9 +1,12 @@
 return {
 	"echasnovski/mini.icons",
-	version = "*",
-	config = function()
-		require("mini.icons").setup()
-
-		MiniIcons.mock_nvim_web_devicons()
+	opts = {},
+	lazy = true,
+	specs = { { "nvim-tree/nvim-web-devicons", enabled = false, optional = true } },
+	init = function()
+		package.preload["nvim-web-devicons"] = function()
+			require("mini.icons").mock_nvim_web_devicons()
+			return package.loaded["nvim-web-devicons"]
+		end
 	end,
 }
