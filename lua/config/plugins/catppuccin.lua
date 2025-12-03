1693eb7aba9f57707fe5b78a68f229e1b9d45a4c
Viet Anh Vo <vietanhvo@pm.me>
2025-12-03 10:51:15 +0700

    Add theme for chrismas

--- lua/config/plugins/catppuccin.lua
+++ lua/config/plugins/catppuccin.lua
@@ -7,9 +7,38 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				background = {
+					light = "latte",
 					dark = "mocha",
 				},
 				color_overrides = {
+					latte = {
+						rosewater = "#c14a4a",
+						flamingo = "#c14a4a",
+						red = "#c14a4a",
+						maroon = "#c14a4a",
+						pink = "#945e80",
+						mauve = "#945e80",
+						peach = "#c35e0a",
+						yellow = "#b47109",
+						green = "#6c782e",
+						teal = "#4c7a5d",
+						sky = "#4c7a5d",
+						sapphire = "#4c7a5d",
+						blue = "#45707a",
+						lavender = "#45707a",
+						text = "#654735",
+						subtext1 = "#73503c",
+						subtext0 = "#805942",
+						overlay2 = "#8c6249",
+						overlay1 = "#8c856d",
+						overlay0 = "#a69d81",
+						surface2 = "#bfb695",
+						surface1 = "#d1c7a3",
+						surface0 = "#e3dec3",
+						base = "#f9f5d7",
+						mantle = "#f0ebce",
+						crust = "#e8e3c8",
+					},
 					mocha = {
 						rosewater = "#ea6962",
 						flamingo = "#ea6962",
@@ -43,19 +72,7 @@ return {
 				show_end_of_buffer = false,
 				integration_default = false,
 				integrations = {
-					cmp = true,
-					gitsigns = true,
-					hop = true,
-					native_lsp = { enabled = true, inlay_hints = { background = true } },
-					nvimtree = true,
-					semantic_tokens = true,
-					treesitter = true,
-					treesitter_context = true,
-					dashboard = true,
-					mason = true,
-					telescope = {
-						enabled = true,
-					},
+					auto_integrations = true,
 				},
 				highlight_overrides = {
 					all = function(colors)
@@ -311,9 +328,15 @@ return {
 						}
 					end,
 				},
-			})
+				latte = function(colors)
+					return {
+						IblIndent = { fg = colors.mantle },
+						IblScope = { fg = colors.surface1 },
 
-			vim.api.nvim_command("colorscheme catppuccin")
+						LineNr = { fg = colors.surface1 },
+					}
+				end,
+			})
 		end,
 	},
 }
